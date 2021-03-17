259324e417870264b2e4eb4651f0973e18039daa
defstat <defstat@gmail.com>
2021-03-17 16:54:00 +0200

    pkp/pkp-lib#6490 Remove loadCLILocales function

--- plugins/importexport/native/NativeImportExportPlugin.inc.php
+++ plugins/importexport/native/NativeImportExportPlugin.inc.php
@@ -70,14 +70,6 @@ class NativeImportExportPlugin extends PKPNativeImportExportPlugin {
 	function getAppSpecificDeployment($journal, $user) {
 		return new NativeImportExportDeployment($journal, $user);
 	}
-
-	/**
-	 * @see PKPNativeImportExportPlugin::loadCLILocales
-	 */
-	function loadCLILocales() {
-		parent::loadCLILocales();
-		AppLocale::requireComponents(LOCALE_COMPONENT_APP_MANAGER);
-	}
 }
 
 
