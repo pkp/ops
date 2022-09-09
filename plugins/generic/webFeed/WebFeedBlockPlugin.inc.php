2e0aa11a2c6a6129d004d17bcfcccd2e4e175bad
Jonas Raoni Soares da Silva <github@raoni.org>
2022-09-09 16:36:59 +0300

    pkp/pkp-lib#7623 Imported web feed plugin from OJS

---
+++ plugins/generic/webFeed/WebFeedBlockPlugin.inc.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * @file plugins/generic/webFeed/WebFeedBlockPlugin.inc.php
+ *
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2003-2021 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class WebFeedBlockPlugin
+ * @ingroup plugins_generic_webFeed
+ *
+ * @brief Class for block component of web feed plugin
+ */
+
+import('lib.pkp.classes.plugins.BlockPlugin');
+
+class WebFeedBlockPlugin extends BlockPlugin {
+	/** @var WebFeedPlugin Parent plugin */
+	protected $_parentPlugin;
+
+	/**
+	 * @param $parentPlugin WebFeedPlugin
+	 */
+	public function __construct($parentPlugin) {
+		parent::__construct();
+		$this->_parentPlugin = $parentPlugin;
+	}
+
+	/**
+	 * Get the name of this plugin. The name must be unique within
+	 * its category.
+	 * @return String name of plugin
+	 */
+	public function getName() {
+		return 'WebFeedBlockPlugin';
+	}
+
+	/**
+	 * Hide this plugin from the management interface (it's subsidiary)
+	 */
+	public function getHideManagement() {
+		return true;
+	}
+
+	/**
+	 * Get the display name of this plugin.
+	 * @return String
+	 */
+	public function getDisplayName() {
+		return __('plugins.generic.webfeed.displayName');
+	}
+
+	/**
+	 * Get a description of the plugin.
+	 */
+	public function getDescription() {
+		return __('plugins.generic.webfeed.description');
+	}
+
+	/**
+	 * Override the builtin to get the correct plugin path.
+	 * @return string
+	 */
+	public function getPluginPath() {
+		return $this->_parentPlugin->getPluginPath();
+	}
+
+	/**
+	 * @copydoc PKPPlugin::getTemplatePath
+	 */
+	public function getTemplatePath($inCore = false) {
+		return $this->_parentPlugin->getTemplatePath($inCore) . '/templates';
+	}
+
+	/**
+	 * Get the HTML contents for this block.
+	 * @param $templateMgr object
+	 * @param $request PKPRequest
+	 * @return $string
+	 */
+	public function getContents($templateMgr, $request = null) {
+		$journal = $request->getJournal();
+		$issueDao = DAORegistry::getDAO('IssueDAO'); /* @var $issueDao IssueDAO */
+		if ($issueDao->getCurrent($journal->getId(), true)) {
+			return parent::getContents($templateMgr, $request);
+		}
+		return '';
+	}
+}
