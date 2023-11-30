86f640003f00a7ebfb42315993ab32b7f10392b8
Vitalii Bezsheiko <vitaliybezsh@gmail.com>
2023-11-30 18:08:44 +0200

    pkp/pkp-lib#7725 Decision migration for OPS

---
+++ classes/migration/upgrade/v3_4_0/I7725_DecisionConstantsUpdate.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @file classes/migration/upgrade/v3_4_0/I7725_DecisionConstantsUpdate.php
+ *
+ * Copyright (c) 2014-2023 Simon Fraser University
+ * Copyright (c) 2000-2023 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class I7725_DecisionConstantsUpdate
+ *
+ * @brief Editorial decision constant sync up across all application
+ *
+ * @see https://github.com/pkp/pkp-lib/issues/7725
+ */
+
+namespace APP\migration\upgrade\v3_4_0;
+
+class I7725_DecisionConstantsUpdate extends \PKP\migration\upgrade\v3_4_0\I7725_DecisionConstantsUpdate
+{
+    public function getDecisionMappings(): array
+    {
+        return [
+            // \PKP\decision\Decision::INITIAL_DECLINE
+            [
+                'stage_id' => WORKFLOW_STAGE_ID_PRODUCTION,
+                'current_value' => 9,
+                'updated_value' => 8,
+            ],
+            // \PKP\decision\Decision::REVERT_INITIAL_DECLINE
+            [
+                'stage_id' => WORKFLOW_STAGE_ID_PRODUCTION,
+                'current_value' => 18,
+                'updated_value' => 16,
+            ]
+        ];
+    }
+}
