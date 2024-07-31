cb3b96fd01b2f4b2288901e56277a7fc541340ec
Jonas Raoni Soares da Silva <github@raoni.org>
2024-07-31 15:19:15 +0300

    pkp/pkp-lib#10249 Cast object to array

--- classes/migration/upgrade/OPSv3_3_0UpgradeMigration.inc.php
+++ classes/migration/upgrade/OPSv3_3_0UpgradeMigration.inc.php
@@ -158,7 +158,7 @@ class OPSv3_3_0UpgradeMigration extends Migration {
 		}
 
 		if (!count($primaryKeys)) {
-			foreach (array_keys($row) as $column) {
+			foreach (array_keys(get_object_vars($row)) as $column) {
 				if (substr($column, -3, '_id')) {
 					$primaryKeys[] = $column;
 				}
