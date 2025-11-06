d4cb58d540b720afc43861ba11049bcae07d5e38
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2025-11-06 10:37:19 +0100

    pkp/pkp-lib#8920 Bring back some smarty variables important to display the date filters.

--- pages/search/SearchHandler.php
+++ pages/search/SearchHandler.php
@@ -22,6 +22,7 @@ use APP\handler\Handler;
 use APP\security\authorization\OpsServerMustPublishPolicy;
 use APP\template\TemplateManager;
 use Laravel\Scout\Builder;
+use PKP\core\PKPRequest;
 use PKP\plugins\Hook;
 use PKP\search\SubmissionSearchResult;
 use PKP\userGroup\UserGroup;
@@ -97,12 +98,12 @@ class SearchHandler extends Handler
         $yearStart = substr($yearRange->min_date_published, 0, 4);
         $yearEnd = substr($yearRange->max_date_published, 0, 4);
 
+        $this->_assignDateFromTo($request, $templateMgr);
+
         $templateMgr->assign([
             'query' => $query,
             'results' => $results,
             'searchContext' => $contextId,
-            'dateFrom' => $dateFrom ? date('Y-m-d H:i:s', $dateFrom) : null,
-            'dateTo' => $dateTo ? date('Y-m-d H:i:s', $dateTo) : null,
             'yearStart' => $yearStart,
             'yearEnd' => $yearEnd,
             'authorUserGroups' => UserGroup::withRoleIds([\PKP\security\Role::ROLE_ID_AUTHOR])
@@ -110,6 +111,46 @@ class SearchHandler extends Handler
                 ->get(),
         ]);
         $templateMgr->display('frontend/pages/search.tpl');
+    }
+
+    /**
+     * Assign dateFrom* and dateTo* variables to template
+     *
+     */
+    public function _assignDateFromTo(PKPRequest $request, TemplateManager &$templateMgr)
+    {
+        // Special case: publication date filters.
+        foreach (['From', 'To'] as $fromTo) {
+            $month = $request->getUserVar("date{$fromTo}Month");
+            $day = $request->getUserVar("date{$fromTo}Day");
+            $year = $request->getUserVar("date{$fromTo}Year");
+            if (empty($year)) {
+                $date = null;
+                $hasEmptyFilters = true;
+            } else {
+                $defaultMonth = ($fromTo == 'From' ? 1 : 12);
+                $defaultDay = ($fromTo == 'From' ? 1 : 31);
+                $date = date(
+                    'Y-m-d H:i:s',
+                    mktime(
+                        0,
+                        0,
+                        0,
+                        empty($month) ? $defaultMonth : $month,
+                        empty($day) ? $defaultDay : $day,
+                        $year
+                    )
+                );
+                $hasActiveFilters = true;
+            }
+
+            $templateMgr->assign([
+                "date{$fromTo}Month" => $month,
+                "date{$fromTo}Day" => $day,
+                "date{$fromTo}Year" => $year,
+                "date{$fromTo}" => $date
+            ]);
+        }
     }
 
     /**
