d3f875ac5418526b9cecf5ffa35ba7670341c5b1
Alec Smecher <alec@smecher.bc.ca>
2026-08-20 11:40:21 -0700

    Fix selector for contributors Order button

--- cypress/tests/data/60-content/CkwantesSubmission.cy.js
+++ cypress/tests/data/60-content/CkwantesSubmission.cy.js
@@ -129,7 +129,7 @@ describe('Data suite: Ckwantes', function() {
 		cy.get('.pkpFormField:contains("Email")').find('input').type(submission.authors[0].email);
 		cy.get('div[role=dialog]:contains("Add Contributor")').find('button').contains('Save').click();
 		cy.wait(3000);
-		cy.get('button').contains('Order').click();
+		cy.get('.contributorsListPanel button:contains("Order")').click();
 		cy.wait(3000);
 		cy.get('button:contains("Decrease position of Catherine Kwantes")').click();
 		cy.get('button').contains('Save Order').click();
