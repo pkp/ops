ab4b990d968fbbf6dc771e38ab00a34104361e58
Vitaliy-1 <vitaliybezsh@gmail.com>
2026-09-08 10:53:46 +0300

    pkp/pkp-lib#13109 Fix canEditPublication() method call

--- controllers/grid/preprintGalleys/PreprintGalleyGridHandler.php
+++ controllers/grid/preprintGalleys/PreprintGalleyGridHandler.php
@@ -510,7 +510,7 @@ class PreprintGalleyGridHandler extends GridHandler
             return true;
         }
 
-        if (Repo::submission()->canEditPublication($submission->getId(), $user->getId())) {
+        if (Repo::submission()->canEditPublication($publication, $user)) {
             return true;
         }
 
